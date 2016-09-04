10a879deeeeddfdc0b21c27663039e85eef38dd6
Justin Cann <email@JustinDCann.com>
2016-09-04 19:46:28 +0100

    Home | Homepage Content | SCSS | Typography | CSS Mixins
    Added CSS Mixins files. Added more styles to typography, added more elements to homepage and customized them in homepage content register

--- library/jdc-register-homepage-content.php
+++ library/jdc-register-homepage-content.php
@@ -35,39 +35,117 @@ function wpt_register_homepage_content( $wp_customize ) {
 	// Settings
 
 
-	// First Service Block Element
+	// Heading
+
+	// First Service Block Heading
 	$wp_customize->add_setting(
-		'wpt_first_service_block_element',
+		'wpt_first_services_block_heading',
+		array(
+			'default'          	=> __( 'Enter in a short heading', 'daisy' ),
+			'sanitize_callback'	=> 'sanitize_text_field',
+		)
+	);
+
+	// Second Service Block Heading
+	$wp_customize->add_setting(
+		'wpt_second_services_block_heading',
+		array(
+			'default'          	=> __( 'Enter in a short heading', 'daisy' ),
+			'sanitize_callback'	=> 'sanitize_text_field',
+		)
+	);
+
+	// Third Service Block Heading
+	$wp_customize->add_setting(
+		'wpt_third_services_block_heading',
+		array(
+			'default'          	=> __( 'Enter in a short heading', 'daisy' ),
+			'sanitize_callback'	=> 'sanitize_text_field',
+		)
+	);
+
+	// Fourth Service Block Heading
+	$wp_customize->add_setting(
+		'wpt_fourth_services_block_heading',
+		array(
+			'default'          	=> __( 'Enter in a short heading', 'daisy' ),
+			'sanitize_callback'	=> 'sanitize_text_field',
+		)
+	);
+
+	// Textarea
+
+	// First Service Block Textarea
+	$wp_customize->add_setting(
+		'wpt_first_services_block_textarea',
 		array(
 			'default'          	=> __( 'Enter in some text for the website', 'daisy' ),
 			'sanitize_callback'	=> 'sanitize_text_field',
 		)
 	);
 
-	// Second Service Block Element
+	// Second Service Block Textarea
 	$wp_customize->add_setting(
-		'wpt_second_service_block_element',
+		'wpt_second_services_block_textarea',
 		array(
 			'default'          	=> __( 'Enter in some text for the website', 'daisy' ),
 			'sanitize_callback'	=> 'sanitize_text_field',
 		)
 	);
 
-	// Third Service Block Element
+	// Third Service Block Textarea
 	$wp_customize->add_setting(
-		'wpt_third_service_block_element',
+		'wpt_third_services_block_textarea',
 		array(
 			'default'          	=> __( 'Enter in some text for the website', 'daisy' ),
 			'sanitize_callback'	=> 'sanitize_text_field',
 		)
 	);
 
-	// Fourth Service Block Element
+	// Fourth Service Block Textarea
 	$wp_customize->add_setting(
-		'wpt_fourth_service_block_element',
+		'wpt_fourth_services_block_textarea',
 		array(
 			'default'          	=> __( 'Enter in some text for the website', 'daisy' ),
 			'sanitize_callback'	=> 'sanitize_text_field',
+		)
+	);
+
+	// Images
+
+	// First Service Block Image
+	$wp_customize->add_setting(
+		'wpt_first_services_block_image',
+		array(
+			'default'          	=> __( 'Select an image for your first service', 'daisy' ),
+			// 'sanitize_callback'	=> 'sanitize_file_name',
+		)
+	);
+
+	// Second Service Block Image
+	$wp_customize->add_setting(
+		'wpt_second_services_block_image',
+		array(
+			'default'          	=> __( 'Select an image for your second service', 'daisy' ),
+			// 'sanitize_callback'	=> 'sanitize_file_name',
+		)
+	);
+
+	// Third Service Block Image
+	$wp_customize->add_setting(
+		'wpt_third_services_block_image',
+		array(
+			'default'          	=> __( 'Select an image for your third service', 'daisy' ),
+			// 'sanitize_callback'	=> 'sanitize_file_name',
+		)
+	);
+
+	// Fourth Service Block Image
+	$wp_customize->add_setting(
+		'wpt_fourth_services_block_image',
+		array(
+			'default'          	=> __( 'Select an image for your fourth service', 'daisy' ),
+			// 'sanitize_callback'	=> 'sanitize_file_name',
 		)
 	);
 
@@ -75,16 +153,72 @@ function wpt_register_homepage_content( $wp_customize ) {
 	//------------------------------------------------------------------------
 	// Controls
 
-	// Add control for first service
+	//-----------------------------
+
+	// Add control for first heading
 	$wp_customize->add_control(
 		new WP_Customize_Control(
 			$wp_customize,
-			'first_services',
+			'first_services_heading',
 			array(
-				'label'    => __( 'First Service', 'daisy' ),
+				'label'    => __( 'First Heading', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_first_services_block_heading',
+			)
+		)
+	);
+
+	// Add control for first image
+	$wp_customize->add_control(
+		new WP_Customize_Image_Control(
+			$wp_customize,
+			'first_services_image',
+			array(
+				'label'    => __( 'First Image', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_first_services_block_image',
+			)
+		)
+	);
+
+	// Add control for first textarea
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'first_services_textarea',
+			array(
+				'label'    => __( 'First Service Text Area', 'daisy' ),
 				'type'		=> 'textarea',
 				'section' 	=> 'services_block_elements',
-				'settings' 	=> 'wpt_first_service_block_element',
+				'settings' 	=> 'wpt_first_services_block_textarea',
+			)
+		)
+	);
+
+	//-----------------------------
+
+	// Add control for second heading
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'second_services_heading',
+			array(
+				'label'    => __( 'Second Heading', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_second_services_block_heading',
+			)
+		)
+	);
+
+	// Add control for second image
+	$wp_customize->add_control(
+		new WP_Customize_Image_Control(
+			$wp_customize,
+			'second_services_image',
+			array(
+				'label'    => __( 'Second Image', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_second_services_block_image',
 			)
 		)
 	);
@@ -93,12 +227,40 @@ function wpt_register_homepage_content( $wp_customize ) {
 	$wp_customize->add_control(
 		new WP_Customize_Control(
 			$wp_customize,
-			'second_services',
+			'second_services_textarea',
 			array(
-				'label'    => __( 'Second Service', 'daisy' ),
+				'label'    => __( 'Second Service Text Area', 'daisy' ),
 				'type'		=> 'textarea',
 				'section' 	=> 'services_block_elements',
-				'settings' 	=> 'wpt_second_service_block_element',
+				'settings' 	=> 'wpt_second_services_block_textarea',
+			)
+		)
+	);
+
+	//-----------------------------
+
+	// Add control for third heading
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'third_services_heading',
+			array(
+				'label'    => __( 'Third Heading', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_third_services_block_heading',
+			)
+		)
+	);
+
+	// Add control for third image
+	$wp_customize->add_control(
+		new WP_Customize_Image_Control(
+			$wp_customize,
+			'third_services_image',
+			array(
+				'label'    => __( 'Third Image', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_third_services_block_image',
 			)
 		)
 	);
@@ -107,12 +269,41 @@ function wpt_register_homepage_content( $wp_customize ) {
 	$wp_customize->add_control(
 		new WP_Customize_Control(
 			$wp_customize,
-			'third_services',
+			'third_services_textarea',
 			array(
-				'label'    => __( 'Third Service', 'daisy' ),
+				'label'    => __( 'Third Service Text Area', 'daisy' ),
 				'type'		=> 'textarea',
 				'section' 	=> 'services_block_elements',
-				'settings' 	=> 'wpt_third_service_block_element',
+				'settings' 	=> 'wpt_third_services_block_textarea',
+			)
+		)
+	);
+
+	//-----------------------------
+
+
+	// Add control for fourth heading
+	$wp_customize->add_control(
+		new WP_Customize_Control(
+			$wp_customize,
+			'fourth_services_heading',
+			array(
+				'label'    => __( 'Fourth Heading', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_fourth_services_block_heading',
+			)
+		)
+	);
+
+	// Add control for fourth image
+	$wp_customize->add_control(
+		new WP_Customize_Image_Control(
+			$wp_customize,
+			'fourth_services_image',
+			array(
+				'label'    => __( 'Fourth Image', 'daisy' ),
+				'section' 	=> 'services_block_elements',
+				'settings' 	=> 'wpt_fourth_services_block_image',
 			)
 		)
 	);
@@ -121,15 +312,17 @@ function wpt_register_homepage_content( $wp_customize ) {
 	$wp_customize->add_control(
 		new WP_Customize_Control(
 			$wp_customize,
-			'fourth_services',
+			'fourth_services_textarea',
 			array(
-				'label'    => __( 'Fourth Service', 'daisy' ),
+				'label'    => __( 'Fourth Service Text Area', 'daisy' ),
 				'type'		=> 'textarea',
 				'section' 	=> 'services_block_elements',
-				'settings' 	=> 'wpt_fourth_service_block_element',
+				'settings' 	=> 'wpt_fourth_services_block_textarea',
 			)
 		)
 	);
+
+	//-----------------------------
 
 }
 
--- page-templates/home.php
+++ page-templates/home.php
@@ -4,7 +4,7 @@ Template Name: Home Page
 */
 get_header(); ?>
 
-<div id="home-page" role="main">
+<main id="home-page" class="home-page" role="main">
 
   <?php do_action('daisy_before_content') ?>
   <?php while ( have_posts() ) : the_post(); ?>
@@ -14,29 +14,84 @@ get_header(); ?>
     </header>
 
 
-    <section class="sevices">
-      <?php// if ( != '' ) : ?>
-        <?php
-        $mods = get_theme_mods();
-        // var_dump($mods);
+    <section class="services">
+      <div class="row" data-equalizer="service-text" data-equalize-on="medium">
+        <div class="large-3 medium-6 small-12 columns">
 
-        // echo  get_theme_mod( 'wpt_services_    block_elements',  );
+          <section class="services">
 
-        // echo $mods ["wpt_third_service_block_element"]
-         ?>
-      <?php// endif ?>
+            <div class="service-image">
+              <img src="<?php echo get_theme_mod( 'wpt_first_services_block_image') ?>" alt="<?php echo get_theme_mod( 'wpt_first_services_block_heading') ?>" />
+            </div>
 
-      <div class="large-3 medium-6 small-12 columns">
-        <?php  echo  get_theme_mod( 'wpt_first_service_block_element')  ?>
-      </div>
-      <div class="large-3 medium-6 small-12 columns">
-        <?php  echo  get_theme_mod( 'wpt_second_service_block_element')  ?>
-      </div>
-      <div class="large-3 medium-6 small-12 columns">
-        <?php  echo  get_theme_mod( 'wpt_third_service_block_element')  ?>
-      </div>
-      <div class="large-3 medium-6 small-12 columns">
-        <?php  echo  get_theme_mod( 'wpt_fourth_service_block_element')  ?>
+            <div class="service-heading heading"><h4><?php echo get_theme_mod( 'wpt_first_services_block_heading') ?></h4></div>
+
+            <div class="service-text" data-equalizer-watch="service-text">
+              <p><?php echo get_theme_mod( 'wpt_first_services_block_textarea') ?></p>
+            </div>
+
+            <div class="read-more"><?php echo _e('Read more') ?></div>
+
+          </section>
+
+        </div>
+        <div class="large-3 medium-6 small-12 columns">
+
+          <section class="services">
+
+            <div class="service-image">
+              <img src="<?php echo get_theme_mod( 'wpt_second_services_block_image') ?>" alt="<?php echo get_theme_mod( 'wpt_second_services_block_heading') ?>" />
+            </div>
+
+            <div class="service-heading heading"><h4><?php echo get_theme_mod( 'wpt_second_services_block_heading') ?></h4></div>
+
+            <div class="service-text" data-equalizer-watch="service-text">
+              <p><?php echo get_theme_mod( 'wpt_second_services_block_textarea') ?></p>
+            </div>
+
+            <div class="read-more"><?php echo _e('Read more') ?></div>
+
+          </section>
+
+        </div>
+        <div class="large-3 medium-6 small-12 columns">
+
+          <section class="services">
+
+            <div class="service-image">
+              <img src="<?php echo get_theme_mod( 'wpt_third_services_block_image') ?>" alt="<?php echo get_theme_mod( 'wpt_third_services_block_heading') ?>" />
+            </div>
+
+            <div class="service-heading heading"><h4><?php echo get_theme_mod( 'wpt_third_services_block_heading') ?></h4></div>
+
+            <div class="service-text" data-equalizer-watch="service-text">
+              <p><?php echo get_theme_mod( 'wpt_third_services_block_textarea') ?></p>
+            </div>
+
+            <div class="read-more"><?php echo _e('Read more') ?></div>
+
+          </section>
+
+        </div>
+        <div class="large-3 medium-6 small-12 columns">
+
+          <section class="services">
+
+            <div class="service-image">
+              <img src="<?php echo get_theme_mod( 'wpt_fourth_services_block_image') ?>" alt="<?php echo get_theme_mod( 'wpt_fourth_services_block_heading') ?>" />
+            </div>
+
+            <div class="service-heading heading"><h4><?php echo get_theme_mod( 'wpt_fourth_services_block_heading') ?></h4></div>
+
+            <div class="service-text" data-equalizer-watch="service-text">
+              <p><?php echo get_theme_mod( 'wpt_fourth_services_block_textarea') ?></p>
+            </div>
+
+            <div class="read-more"><?php echo _e('Read more') ?></div>
+
+          </section>
+
+        </div>
       </div>
     </section>
 
@@ -44,6 +99,6 @@ get_header(); ?>
   <?php endwhile;?>
   <?php do_action('daisy_after_content') ?>
 
-</div>
+</main>
 
 <?php get_footer();
